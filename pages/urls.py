c5441137451a51113cb63502645fe1889fdc604a
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2024-08-29 22:23:11 -0500

    sitemap

--- pages/urls.py
+++ pages/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('events', views.events, name='events'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
+    path('sitemap', views.sitemap, name='sitemap'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Contact
 from portal.models import CarouselImage
 
@@ -7,12 +7,11 @@ def index(request):
     images = images.order_by("order")
     return render(request,"index.html", {"images":images})
 
-
-
 def events(request):
     return render(request,"events.html")
 
-
+def sitemap(request):
+    return HttpResponse(open('templates/sitemap.xml').read(), content_type='text/xml')
 
 def about(request):
     return render(request,"about.html")
